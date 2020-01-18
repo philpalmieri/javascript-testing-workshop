a774c4ae5c8b5e3f9e4af0d354f6d87f7cd791e9
Phil Palmieri <phil@palmierimail.com>
2020-01-18 14:18:02 -0500

    Add uppercase

--- hello-there/hello.js
+++ hello-there/hello.js
@@ -1,3 +1,7 @@
-const hello = (name) => `hello, ${name}`
+const capitalize = (s) => {
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
+
+const hello = (name) => `Hello, ${capitalize(name)}`
 
 exports.hello = hello
--- hello-there/tests/hello.test.js
+++ hello-there/tests/hello.test.js
@@ -6,9 +6,15 @@ describe('hello module', () => {
     //const returnedName = hello(testName)
     //expect(returnedName).toBe(returnedName)
   //})
-  test('name returns with hello, name', () => {
+  //test('name returns with hello, name', () => {
+    //const testName = 'joe'
+    //const returnedName = hello(testName)
+    //expect(returnedName).toBe(`hello, ${testName}`)
+  //})
+  test('name returns with Hello, Name', () => {
     const testName = 'joe'
+    const expectedName = 'Joe'
     const returnedName = hello(testName)
-    expect(returnedName).toBe(`hello, ${testName}`)
+    expect(returnedName).toBe(`Hello, ${expectedName}`)
   })
 })
