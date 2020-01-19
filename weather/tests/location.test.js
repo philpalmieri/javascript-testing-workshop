3fc562a406de186c39669f09e934dec8983e7a15
Phil Palmieri <phil@palmierimail.com>
2020-01-19 07:35:26 -0500

    cleanup tests

--- weather/tests/location.test.js
+++ weather/tests/location.test.js
@@ -1,6 +1,5 @@
 describe('location connection', () => {
   test('it worked', () => {
-    console.log(process.env)
-    expect(true).toBe(true)
+    expect(process.env.TESTING).toBe("true")
   })
 })
--- weather/tests/setup.js
+++ weather/tests/setup.js
@@ -1,2 +1,2 @@
 console.log('Loading .env file for tests')
-require('dotenv').config({ path: '.env.test' })
+require('dotenv').config({ path: './tests/.env.test' })
