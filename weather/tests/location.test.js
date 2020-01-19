aac2543cf2e0b01b31ad7a51e71039b30c6a93ac
Phil Palmieri <phil@palmierimail.com>
2020-01-19 08:09:09 -0500

    working nock

--- weather/tests/location.test.js
+++ weather/tests/location.test.js
@@ -1,5 +1,20 @@
-describe('location connection', () => {
+const nock = require('nock')
+const { getLocation } = require('../location')
+
+describe('env config works', () => {
   test('it worked', () => {
     expect(process.env.TESTING).toBe("true")
+  })
+})
+
+describe('location connection', () => {
+  test('city is sent', async () => {
+    nock(process.env.OPENCAGE_URL)
+      .get('/json')
+      .query({ key: process.env.OPENCAGE_KEY, q: 'Orlando' })
+      .reply(200, {hello: 'test'})
+    const response = await getLocation('Orlando')
+    console.log(response)
+    expect(true).toBe(true)
   })
 })
