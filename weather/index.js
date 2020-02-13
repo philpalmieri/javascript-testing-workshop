12f60d1e4248d32b811c378beeb35efcacfb5085
Phil Palmieri <philthedev@gmail.com>
2020-02-13 18:24:59 -0500

    Revert "added console.log message (#1)"
    This reverts commit b2c2ada9f1fdfc08d46e425c179aad359ab4fac9.

--- weather/index.js
+++ weather/index.js
@@ -1,29 +1,28 @@
-require("dotenv").config();
-const program = require("commander");
+require('dotenv').config()
+const program = require('commander')
 
-const { getLocation } = require("./location");
+const { getLocation } = require('./location')
 
-program.option("-c, --city <string>", "where do you live");
-program.parse(process.argv);
+program.option('-c, --city <string>', 'where do you live')
+program.parse(process.argv)
 
 if (!process.argv.slice(2).length) {
-  program.outputHelp();
-  process.exit();
+  program.outputHelp()
+  process.exit()
 }
 
 // Application Logic
 
-const run = async () => {
+const run = async() => {
   try {
-    const cityData = await getLocation(program.city);
-    const { results } = JSON.parse(cityData);
+    const cityData = await getLocation(program.city)
+    const { results } = JSON.parse(cityData); 
     //console.log(JSON.stringify(results[0], null, 2))
   } catch (e) {
-    console.error(e);
+    console.error(e) 
   }
-};
+}
 
-if (program.city != "") {
-  console.log("added console 2/13/2020");
-  run();
+if(program.city != '') {
+  run()
 }
